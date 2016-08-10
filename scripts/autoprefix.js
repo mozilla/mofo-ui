c62f12c3df2b54730e43b922aec01f47b14dda5d
gavin lazar suntop <gavin@gsuntop.com>
2016-08-10 11:46:27 -0500

    adding autoprefixer to build process (#35)

---
+++ scripts/autoprefix.js
@@ -0,0 +1,17 @@
+let autoprefixer = require(`autoprefixer`);
+let postcss = require(`postcss`);
+let shelljs = require(`shelljs`);
+
+let cssPath = `dist/mofo-ui.css`;
+let css = shelljs.cat(cssPath).toString();
+
+postcss([ autoprefixer ]).process(css).then((result) => {
+  result.warnings().forEach((warn) => {
+    console.warn(warn.toString());
+    shelljs.exit(1);
+  });
+
+  console.log(`Autoprefixed CSS: ${cssPath}`);
+  shelljs.ShellString(result.css).to(cssPath);
+  shelljs.exit(0);
+});
